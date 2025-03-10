599fba6fcc41dd62cb920df4ae0fe471fb3c687f
Natasza ASzatan <natiye.nb@gmai.com>
2025-03-10 01:48:59 +0100

    added missing controller in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { UsersService } from './users.service';
+import {UsersController} from "./users.controller";
 
 @Module({
   imports: [DatabaseModule],
   providers: [UsersService],
   exports: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
