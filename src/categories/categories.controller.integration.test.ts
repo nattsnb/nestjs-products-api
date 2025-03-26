7e5f79cc49a14531589ba5bfcf8de5c6466cb634
Natasza ASzatan <natiye.nb@gmai.com>
2025-03-26 13:59:34 +0100

    created two tests for categoriesController

---
+++ src/categories/categories.controller.integration.test.ts
@@ -0,0 +1,106 @@
+import { ExecutionContext, INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../database/prisma.service';
+import { CategoriesController } from './categories.controller';
+import { Category } from '@prisma/client';
+import * as request from 'supertest';
+import { CategoriesService } from './categories.service';
+import { ProductsService } from '../products/products.service';
+import { JwtAuthenticationGuard } from '../authentication/jwt-authentication.guard';
+import { CreateCategoryDto } from './create-category.dto';
+
+describe('The CategoriesController', () => {
+  let app: INestApplication;
+  let findUniqueMock: jest.Mock;
+  let createMock: jest.Mock;
+  beforeEach(async () => {
+    findUniqueMock = jest.fn();
+    createMock = jest.fn();
+    const module = await Test.createTestingModule({
+      providers: [
+        CategoriesService,
+        {
+          provide: PrismaService,
+          useValue: {
+            category: {
+              findUnique: findUniqueMock,
+              create: createMock,
+            },
+          },
+        },
+        {
+          provide: ProductsService,
+          useValue: {},
+        },
+      ],
+      controllers: [CategoriesController],
+      imports: [],
+    })
+      .overrideGuard(JwtAuthenticationGuard)
+      .useValue({
+        canActivate: (context: ExecutionContext) => {
+          const req = context.switchToHttp().getRequest();
+          req.user = {
+            id: 1,
+            name: 'John Smith',
+          };
+          return true;
+        },
+      })
+      .compile();
+
+    app = module.createNestApplication();
+    await app.init();
+  });
+  describe('The CategoriesController', () => {
+    describe('when the GET /categories/:id endpoint is called', () => {
+      let category: Category;
+      beforeEach(() => {
+        category = {
+          id: 1,
+          name: 'My category',
+        };
+        findUniqueMock.mockImplementation((args: { where: { id: number } }) => {
+          if (args.where.id === 1) {
+            return Promise.resolve(category);
+          }
+        });
+      });
+      describe('and the category with a given id exists', () => {
+        it('should respond with the category', () => {
+          return request(app.getHttpServer())
+            .get('/categories/1')
+            .expect(category);
+        });
+      });
+      describe('and the category with a given id does not exist', () => {
+        it('should respond with the 404 status', () => {
+          return request(app.getHttpServer()).get('/categories/2').expect(404);
+        });
+      });
+    });
+    describe('and the POST /categories endpoint is called', () => {
+      describe('and the correct data is provided', () => {
+        let categoryData: CreateCategoryDto;
+        beforeEach(() => {
+          categoryData = {
+            name: 'New category',
+          };
+          createMock.mockResolvedValue({
+            id: 2,
+            ...categoryData,
+          });
+        });
+        it('should respond with the new category', () => {
+          return request(app.getHttpServer())
+            .post('/categories')
+            .send(categoryData)
+            .expect({
+              id: 2,
+              ...categoryData,
+            });
+        });
+      });
+    });
+  });
+});
